b75bbcc9c0344f463a1bd5b322944f901318ab7c
Mario Naranjo <mario15695@hotmail.com>
2016-04-06 11:23:18 -0600

    BEHAVIOUR STRATEGY

---
+++ COMPORTAMIENTO/Strategy/CarBrake/src/carbrake/CarBrake.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carbrake;
+
+/**
+ *
+ * @author jonaranjo
+ */
+public class CarBrake {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
