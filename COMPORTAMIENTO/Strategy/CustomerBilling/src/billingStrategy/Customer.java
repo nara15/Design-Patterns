2be693a96b34452a7d39f00268d82315f566299a
Mario Naranjo <mario15695@hotmail.com>
2016-04-06 11:36:05 -0600

    Context

---
+++ COMPORTAMIENTO/Strategy/CustomerBilling/src/billingStrategy/Customer.java
@@ -0,0 +1,45 @@
+
+package billingStrategy;
+
+import java.util.List;
+
+/**
+ *
+ * @author jonaranjo
+ */
+public class Customer {
+    
+    private List<Double> _drinksPrices;
+    private IBillingStrategy _strategy;
+
+    public Customer(IBillingStrategy pBillingStrategy) 
+    {
+        this._strategy = pBillingStrategy;
+    }
+
+    public void setStrategy(IBillingStrategy pBillingStrategy) 
+    {
+        this._strategy = pBillingStrategy;
+    }
+    
+    public void add(double pDrinkPrice, int pQuantity)
+    {
+        double totalDrinkPrice = pDrinkPrice * pQuantity;
+        _drinksPrices.add(_strategy.getPrice(totalDrinkPrice));
+    }
+    
+    public void printBill()
+    {
+        double sum = 0;
+        for (Double price : _drinksPrices)
+        {
+            sum += price;
+        }
+        System.out.println("Total due: " + sum);
+        _drinksPrices.clear();
+    }
+    
+    
+    
+    
+}
