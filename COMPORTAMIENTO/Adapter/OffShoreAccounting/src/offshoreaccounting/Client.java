df58c2323113ec25e2230a0edd702793b08962ce
Mario Naranjo <mario15695@hotmail.com>
2016-05-09 10:55:27 -0600

    Adapter

--- COMPORTAMIENTO/Adapter/OffShoreAccounting/src/offshoreaccounting/Client.java
+++ COMPORTAMIENTO/Adapter/OffShoreAccounting/src/offshoreaccounting/Client.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package offshoreaccounting;
 
 /**
@@ -15,7 +11,7 @@ public class Client {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // http://javatechig.com/design-patterns/adapter-design-pattern-in-java
     }
     
 }
